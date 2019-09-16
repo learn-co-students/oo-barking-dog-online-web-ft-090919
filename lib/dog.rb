5c7320a62f04db3c04a69b192fb8e7c4de4567ef
Jennifer Nicely <pipeline38@hotmail.com>
2019-09-16 16:47:08 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,16 @@
-# Your code goes here!
+class Dog
+  
+  def bark
+    puts "woof!"
+  end 
+  
+  def name=(dog_name)
+    @dogs_name = dog_name
+  end 
+  
+  def name
+    @dogs_name
+  end 
+  
+end
+  
